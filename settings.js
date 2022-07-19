3e087f56d06cdccd2e4c1348c99b718d991c0505
Dulabot355 <109622794+Dulabot355@users.noreply.github.com>
2022-07-20 03:34:50 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -32,13 +32,13 @@ global.APIKeys = {
 }
 
 //other
-global.owner = ['94715166712'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
+global.owner = ['12366012568'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
 global.pemilik = ['94715166712'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
 global.premium = ['94715166712'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
 global.pengguna = 'Dark Maker' // Your name ඔබේ නම 
-global.botnma = '💃 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐁𝐨𝐭 💃' // Your bot name , ඔබගෙ බොට්ගේ නම 
-global.ownernma = '𝓓𝓪𝓻𝓴 𝓜𝓪𝓴𝓮𝓻' //ownernama,ownername
-global.packname = 'Queen Elisa Sticker' // Sticker package name 
+global.botnma = '💃 𝐐𝐮𝐞𝐞𝐧 𝗦𝗮𝗻𝗱𝘂 𝐁𝐨𝐭 💃' // Your bot name , ඔබගෙ බොට්ගේ නම 
+global.ownernma = '𝙳𝚄 𝙻𝙰' //ownernama,ownername
+global.packname = 'Queen 𝚂𝚊𝚗𝚍𝚞 Sticker' // Sticker package name 
 global.author = 'Dark Maker' // Sticker Autor name 
 global.sessionName = 'session'
 global.prefa = ['#','!','/',''] 
