9ec8a85bb0bbdb12675f0f5587a3198cdd02a7f5
shaharyaro <112632816+shaharyaro@users.noreply.github.com>
2022-09-10 09:28:22 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -32,9 +32,9 @@ global.APIKeys = {
 }
 
 //other
-global.owner = ['94715166712'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
-global.pemilik = ['94715166712'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
-global.premium = ['94715166712'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
+global.owner = ['03093768677'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
+global.pemilik = ['03093768677'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
+global.premium = ['03093768677'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
 global.pengguna = 'Dark Maker' // Your name ඔබේ නම 
 global.botnma = '💃 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐁𝐨𝐭 💃' // Your bot name , ඔබගෙ බොට්ගේ නම 
 global.ownernma = '𝓓𝓪𝓻𝓴 𝓜𝓪𝓴𝓮𝓻' //ownernama,ownername
@@ -44,7 +44,7 @@ global.sessionName = 'session'
 global.prefa = ['#','!','/',''] 
 global.sp = '🔵'
 // Alive massage , ඔබේ බොගේ alive massage  එක මෙහි ඇති ` ` මෙම ලකුනු ඇතුලේ ඔබට අවශ්‍ය ඔනිම දෙයක් Type කරන්න.💃
-global.alivelogo = `https://telegra.ph/file/1a2dee71baa975091620d.jpg`
+global.alivelogo = `https://chat.whatsapp.com/JJRknujBKzbC76Svk5OrfW
 global.alive =`Hello i am alive now
 
 Download song type .yt 
